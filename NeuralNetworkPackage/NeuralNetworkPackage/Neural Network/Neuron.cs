ba5dbfd9e1676dc7341ede725e800ba3bfae968c
Ahmed Hani <ahani.fcis@gmail.com>
2015-04-11 05:56:50 +0200

    - Bugs Fixing

--- NeuralNetworkPackage/NeuralNetworkPackage/Neural Network/Neuron.cs
+++ NeuralNetworkPackage/NeuralNetworkPackage/Neural Network/Neuron.cs
@@ -4,109 +4,111 @@ using System.Collections.Generic;
 
 namespace NeuralNetworkPackage
 {
-	public class Neuron
-	{
-		private List <double> weights;
-		private double bias;
-		private List <double> input;
-		private double net;
-		private double output;
-		private mathFunction activationFunction;
-		private double signalError;
+    public class Neuron
+    {
+        private List<double> weights;
+        private double bias;
+        private List<double> input;
+        private double net;
+        private double output;
+        private mathFunction activationFunction;
+        private double signalError;
 
-		public List <double> Weights
-		{
-			get { return this.weights; }
-		}
-		public double Bias
-		{
-			get { return this.bias; }
-		}
-		public List <double> Input 
-		{
-			get { return this.input; }
-		}
-		public double Net
-		{
-			get { return this.net;}
-		}
-		public double Output 
-		{
-			get { return this.output; }
-		}
-		public double SignalError
-		{
-			set { this.signalError = value; }
-			get { return this.signalError; }
-		}
-		public mathFunction ActivationFunction
-		{
-			get { return this.activationFunction; }
-		}
+        public List<double> Weights
+        {
+            get { return this.weights; }
+        }
+        public double Bias
+        {
+            get { return this.bias; }
+        }
+        public List<double> Input
+        {
+            get { return this.input; }
+        }
+        public double Net
+        {
+            get { return this.net; }
+        }
+        public double Output
+        {
+            get { return this.output; }
+        }
+        public double SignalError
+        {
+            set { this.signalError = value; }
+            get { return this.signalError; }
+        }
+        public mathFunction ActivationFunction
+        {
+            get { return this.activationFunction; }
+        }
 
-		/*Constructors*/
-		public Neuron (int numOfinput, mathFunction activationFunction)
-		{
-			this.activationFunction = activationFunction;
-			this.init (numOfinput);
-		}
-		public Neuron (List <double> weights, mathFunction activationFunction)
-		{
-			this.activationFunction = activationFunction;
-			this.init (weights.Count);
+        /*Constructors*/
+        public Neuron(int numOfinput, mathFunction activationFunction)
+        {
+            this.activationFunction = activationFunction;
+            this.init(numOfinput);
+        }
+        public Neuron(List<double> weights, mathFunction activationFunction)
+        {
+            this.activationFunction = activationFunction;
+            this.init(weights.Count);
 
-			this.weights = weights;
-		}
-		public Neuron (List <double> weights, double bias, mathFunction activationFunction)
-		{
-			this.activationFunction = activationFunction;
-			this.init (weights.Count);
+            this.weights = weights;
+        }
+        public Neuron(List<double> weights, double bias, mathFunction activationFunction)
+        {
+            this.activationFunction = activationFunction;
+            this.init(weights.Count);
 
-			this.weights = weights;
-			this.bias = bias;
-		}
+            this.weights = weights;
+            this.bias = bias;
+        }
 
-		private void init (int numOfinput)
-		{
-			this.weights = new List <double> ();
-			this.input = new List <double> ();
+        private void init(int numOfinput)
+        {
+            this.weights = new List<double>();
+            this.input = new List<double>();
 
-			for (int i = 0; i < numOfinput; ++i){
-				this.weights.Add (0);
-				this.input.Add (0);
-			}
-			this.bias = 0;
-			this.output = 0;
-			this.signalError = 0;
-		}
+            for (int i = 0; i < numOfinput; ++i)
+            {
+                this.weights.Add(0);
+                this.input.Add(0);
+            }
+            this.bias = 0;
+            this.output = 0;
+            this.signalError = 0;
+        }
 
-		private double linearCalculation ()
-		{
-			int size = this.weights.Count;
-			double result = 0;
+        private double linearCalculation()
+        {
+            int size = this.weights.Count;
+            double result = 0;
 
-			for (int i = 0; i < size; ++i) {
-				result += (this.input [i] * this.weights [i]);
-			}
-			// Adding Bias
-			result += bias;
+            for (int i = 0; i < size; ++i)
+            {
+                result += (this.input[i] * this.weights[i]);
+            }
+            // Adding Bias
+            result += bias;
 
-			return result;
-		}
-		public double feedforward (List <double> input)
-		{
-			this.input = input;
+            return result;
+        }
+        public double feedforward(List<double> input)
+        {
+            this.input = input;
 
-			this.net = this.linearCalculation ();
-			this.output = this.activationFunction.function (this.net);
+            this.net = this.linearCalculation();
+            this.output = this.activationFunction.function(this.net);
 
-			return this.output;
-		}
-		public void update (List <double> weights, double bias)
-		{
-			this.weights = weights;
-			this.bias = bias;
-		}
-	}
+            return this.output;
+        }
+        public void update(List<double> weights, double bias)
+        {
+            this.weights = weights;
+            this.bias = bias;
+        }
+    }
 }
 
